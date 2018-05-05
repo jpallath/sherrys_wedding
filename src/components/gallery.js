782e92a5d5e524dbac9fc75fce4640eaaa7ee71b
Jerry Pallath <jpallath@nyu.edu>
2018-05-05 17:26:25 -0400

    fixes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
-import beach from "../images/beach.jpg";
-import engagement from "../images/engagement.jpg";
+import axios from "axios";
+
+import PhotoItem from "./photoitem";
+import "../styles/gallery.css";
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      apiCaller:
+        "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=2ec1ee04d803c2cfa9bf388963cd3bee&photoset_id=72157668635132158&user_id=157021524%40N06&format=json&nojsoncallback=1&api_sig=d7c1e6c1fbcc6f0800b55e58bc7d3678"
+    };
+  }
+  componentDidMount() {
+    this.getPhotos();
+  }
+  getPhotos() {
+    axios.get(this.state.apiCaller).then(res => {
+      let photos = this.state.photos.concat(res.data.photoset.photo);
+      this.setState({ photos: photos });
+    });
+  }
+
   render() {
-    const arrayOfPictures = [{ beach }];
-    const pictures = arrayOfPictures.map((image, index) => <div>...image</div>);
-    return <div className="gallery">{pictures}</div>;
+    let { photos } = this.state;
+    let photoSet = photos.map(photo => <PhotoItem {...photo} key={photo.id} />);
+    return <div className="photoset">{photoSet}</div>;
   }
 }
 
---
+++ src/components/photoitem.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PhotoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { id, farm, secret, server } = this.props;
+    const source = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_h.jpg`;
+    return <img className="photo-item" src={source} />;
+  }
+}
+
+export default PhotoItem;
