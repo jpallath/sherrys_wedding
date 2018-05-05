3607664257569791bf33a5fdccc06b7ca11e8e52
Jerry Pallath <jpallath@nyu.edu>
2018-05-05 09:13:10 -0400

    clean slate

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import beach from "../images/beach.jpg";
+import engagement from "../images/engagement.jpg";
 
 class Gallery extends Component {
   render() {
-    return <div>Here is the gallery</div>;
+    const arrayOfPictures = [{ beach }];
+    const pictures = arrayOfPictures.map((image, index) => <div>...image</div>);
+    return <div className="gallery">{pictures}</div>;
   }
 }
 
