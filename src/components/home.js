712c63cc33570d37ca1110be6a2bbe4e4a915653
Jerry Pallath <jpallath@nyu.edu>
2018-05-07 14:38:51 -0400

    one page

--- src/components/home.js
+++ src/components/home.js
@@ -11,13 +11,13 @@ class Home extends Component {
             <h1>#Jonesin4Sherry </h1>
             <p>
               Thank you for all your love, support and prayers throughout our
-              relationship. We are thrilled to share the details of our wedding
-              with you so please check back frequently for updates regarding our
-              destination wedding!
+              adventure together from day one. We are thrilled to share the
+              details of our coming wedding with you. Please check back
+              frequently for updates regarding our destination wedding!
             </p>
           </div>
           <div className="parallax image-two" />
-          <h1>He proposed!</h1>
+          <h1>#Jonesin4Sherry</h1>
         </div>
       </div>
     );
