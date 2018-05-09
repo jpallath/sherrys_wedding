3014e9abe80ad6b09f696468d89da79895ad2776
Jerry Pallath <jpallath@nyu.edu>
2018-05-09 09:43:23 -0400

    new images plus fixed up story look for now

--- src/components/addtoourstory.js
+++ src/components/addtoourstory.js
@@ -7,7 +7,7 @@ class AddtoOurStory extends Component {
     return (
       <div className="home">
         <div className="details-greeter">
-          <div className="parallax image-one" />
+          <div className="parallax image-three" />
           <div className="home-text">
             <h1>Our Story</h1>
             <p>
@@ -17,16 +17,16 @@ class AddtoOurStory extends Component {
               take a flight out to Newark, New Jersey. Sherry made the 2 hour
               trip from Philadelphia, Pennsylvania to pick up (not knowing at
               the time) her future fiancé. The 2-hour trip the couple finally
-              talked side face to side face and checking each other out
-              sneakingly. That weekend June 9 -11 2017 the couple was themselves
-              for the first time but still had the goosebumps every time the
-              other looked at them. From June each would be traveling from coast
-              to coast.. July 4, 2017, we fell in love each other on a phone
-              call. Jones fell deeply in love and was ready to lock her down and
-              spend the rest of their lives together.
+              talked while sneakily checking each other out with side glances
+              That weekend June 9 -11 2017 the couple was themselves for the
+              first time but still had the goosebumps every time the other
+              looked at them. From June each would be traveling from coast to
+              coast.. July 4, 2017, we fell in love each other on a phone call.
+              Jones fell deeply in love and was ready to lock her down and spend
+              the rest of their lives together.
             </p>
           </div>
-          <div className="parallax image-two" />
+          <div className="parallax image-four" />
           <h1 className="hashtag">#Jonesin4Sherry</h1>
         </div>
       </div>
