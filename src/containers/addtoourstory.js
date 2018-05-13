62d995d0d8cc3b8b95209273bf858d1bb13fb63a
Jerry Pallath <jpallath@nyu.edu>
2018-05-13 00:34:05 -0400

    fixes

--- src/containers/addtoourstory.js
+++ src/containers/addtoourstory.js
@@ -12,21 +12,36 @@ class AddtoOurStory extends Component {
           <div className="image">
             <img src={storyOne} alt="first" />
           </div>
-          <p>
-            May 01, 2017 marks the date when the couple first started texting.
-            Texting lead to calls then video calls (the occasional snapchat)
-            then finally the connection was strong enough for Jones to finally
-            take a flight out to Newark, New Jersey. Sherry made the 2 hour trip
-            from Philadelphia, Pennsylvania to pick up (not knowing at the time)
-            her future fiancé. The 2-hour trip the couple finally talked side
-            face to side face and checking each other out sneakingly. That
-            weekend June 9 -11 2017 the couple was themselves for the first time
-            but still had the goosebumps every time the other looked at them.
-            From June each would be traveling from coast to coast.. July 4,
-            2017, we fell in love each other on a phone call. Jones fell deeply
-            in love and was ready to lock her down and spend the rest of their
-            lives together.
-          </p>
+          <div className="storytime">
+            <p>
+              <span> May, 01, 2017 </span> marks the moment when the first text
+              was sent. When things feel right, texting evolves into SnapChats,
+              which then turns phone calls, and then into video chats, until
+              ultimately seeing each other face to face, no barriers.
+            </p>
+            <p>
+              <span> Jones </span> finally took a flight out to Newark, New
+              Jersey. <span>Sherry</span> made the 2 hour drive from
+              Philadelphia to meet her [potential] fiancé. That first car trip
+              back to Philadelphia was the first time for both to really get to
+              know each other face to face (or well, side of the face to side of
+              the face)…and subtly check the other one out, too.
+            </p>
+
+            <p>
+              <span>That weekend, June 2017, </span> the two felt like
+              themselves around the other— but they still had goosebumps
+              whenever the other looked at them. Since that weekend, each of
+              them would travel to the other side of the country, coast to
+              coast.
+            </p>
+
+            <p>
+              <span>July 4, 2017,</span> we fell for each during a phone call.
+              Jones fell deeply in love and was ready to lock her down and put a
+              💍 on it , and spend the rest of their lives together.
+            </p>
+          </div>
         </div>
         <h1 className="hashtag">#Jonesin4Sherry</h1>
       </div>
